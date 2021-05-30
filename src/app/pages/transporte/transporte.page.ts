ca1bcfc1af885085905d9c6145c43449f00962d0
Daniel Gotardo <profe.gotardo@gmail.com>
2021-05-30 17:59:57 -0300

    telas

--- src/app/pages/transporte/transporte.page.ts
+++ src/app/pages/transporte/transporte.page.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportePage implements OnInit {
 
+  contatoForm: boolean;
+  tipoForm: boolean;
+  solicitaForm: boolean;
+  default: boolean;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ionViewWillEnter() {
+    this.default = true;
+    this.contatoForm = false;
+    this.tipoForm = false;
+    this.solicitaForm = false;
+  }
+
+  segmentChanged(ev: any) {
+    let param = ev.detail.value
+    this.contatoForm = false;
+    this.tipoForm = false;
+    this.solicitaForm = false;
+    this.default = true;
+
+    if (param == 'contato') {
+      this.contatoForm = true;
+      this.default = false;
+    }
+    if (param == 'tipo') {
+      this.tipoForm = true;
+      this.default = false;
+    }
+    if (param == 'solicitacao') {
+      this.solicitaForm = true;
+      this.default = false;
+    }
+
   }
 
 }
